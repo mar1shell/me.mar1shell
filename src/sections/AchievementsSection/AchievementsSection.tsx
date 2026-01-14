ae79fdfb8ea5ad0f7f9d27e28fe2513efe2b4259
mar1shell <elhamdaouimar1@gmail.com>
2026-01-14 14:08:57 +0100

    feat: add AchievementsSection component to display achievements

---
+++ src/sections/AchievementsSection/AchievementsSection.tsx
@@ -0,0 +1,11 @@
+import SectionTitle from "../../components/SectionTitle/SectionTitle";
+
+function AchievementsSection() {
+  return (
+    <section id="achievements-section" className="max-w-5xl px-4 py-6">
+      <SectionTitle title="Achievements" />
+    </section>
+  );
+}
+
+export default AchievementsSection;
